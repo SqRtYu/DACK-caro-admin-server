1fb4fddecb1dd066031a97c10cc4f2ce43a5ccf9
Tran Quoc Toan <37733986+yato511@users.noreply.github.com>
2020-12-09 20:34:39 +0700

    fix condition

--- controllers/admins-controllers.js
+++ controllers/admins-controllers.js
@@ -1,76 +1,73 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
-const User = require('../models/user');
-const Admin = require('../models/admin');
+const User = require("../models/user");
+const Admin = require("../models/admin");
 
 const getUsers = async (req, res, next) => {
-  let users;
+	let users;
 
-  try {
-    users = await User.find({}, '-password');
-  } catch (err) {
-    const error = new HttpError(
-      'Fetching users failed, please try again later.',
-      500
-    );
-    return next(error);
-  }
+	try {
+		users = await User.find({}, "-password");
+	} catch (err) {
+		const error = new HttpError(
+			"Fetching users failed, please try again later.",
+			500
+		);
+		return next(error);
+	}
 
-  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
+	res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const login = async (req, res, next) => {
-    const { userName, password } = req.body;
-  
-    let existingUser;
-  
-    try {
-      existingUser = await Admin.findOne({ userName: userName });
-    } catch (err) {
-      const error = new HttpError('Logging fail please try again.', 500);
-      return next(error);
-    }
+	const { userName, password } = req.body;
 
-  
-    if (!existingUser) {
-      const error = new HttpError(
-        'Invalid credentials, could not log you in.',
-        403
-      );
-      return next(error);
-    }
-    
+	let existingUser;
 
-    if (!password === existingUser.password) {
-      const error = new HttpError(
-        'Invalid credentials, could not log you in.',
-        500
-      );
-      return next(error);
-    }
-  
-    let token;
-    try {
-      token = jwt.sign(
-        {
-          userId: existingUser.id,
-          userName: existingUser.userName,
-        },
-        process.env.JWT_KEY,
-        { expiresIn: '1h' }
-      );
-    } catch (err) {
-      const error = new HttpError('Logging fail please try again.', 500);
-      return next(error);
-    }
-  
-    res.json({
-      userId: existingUser.id,
-      userName: existingUser.userName,
-      email: existingUser.email,
-      token: token,
-    });
-  };
+	try {
+		existingUser = await Admin.findOne({ userName: userName });
+	} catch (err) {
+		const error = new HttpError("Logging fail please try again.", 500);
+		return next(error);
+	}
+
+	if (!existingUser) {
+		const error = new HttpError(
+			"Invalid credentials, could not log you in.",
+			403
+		);
+		return next(error);
+	}
+
+	if (password !== existingUser.password) {
+		const error = new HttpError(
+			"Invalid credentials, could not log you in.",
+			500
+		);
+		return next(error);
+	}
+
+	let token;
+	try {
+		token = jwt.sign(
+			{
+				userId: existingUser.id,
+				userName: existingUser.userName,
+			},
+			process.env.JWT_KEY
+		);
+	} catch (err) {
+		const error = new HttpError("Logging fail please try again.", 500);
+		return next(error);
+	}
+
+	res.json({
+		userId: existingUser.id,
+		userName: existingUser.userName,
+		email: existingUser.email,
+		token: token,
+	});
+};
 
 exports.getUsers = getUsers;
 exports.login = login;
